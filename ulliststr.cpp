1dabbeae91a4cd3f7fe7612df457743dd496c0d6
Yunting Ma <yuntingm@usc.edu>
2022-09-10 05:30:49 +0000

    Check in changes for problem 2

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,145 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val)
+{
+	//Adds a new value to the back of the list
+	size_ += 1;
+	if (size_ == 1) { //when no existing list
+		Item* new_Item = new Item(); //create a new list
+		new_Item->last = 1;
+		new_Item->val[0] = val;
+		head_ = new_Item;
+		tail_ = new_Item;
+		return;
+	}
+	if (tail_->last==ARRSIZE) { //when current is full
+		Item* new_Item = new Item(); //create a new list
+		tail_->next = new_Item;
+		new_Item->prev = tail_;
+		tail_ = new_Item;
+		new_Item->last = 1;
+		new_Item->val[0] = val;
+	}
+	else {
+		tail_->val[tail_->last] = val;
+		tail_->last += 1;
+	}
+	return;
+}
+
+void ULListStr::push_front(const std::string& val)
+{
+	//Adds a new value to the front of the list.
+	size_ += 1;
+	if (size_ == 1) { //when no existing list
+		Item* new_Item = new Item(); //create a new list
+		new_Item->last = ARRSIZE;
+		new_Item->first = ARRSIZE-1;
+		new_Item->val[ARRSIZE-1] = val;
+		head_ = new_Item;
+		tail_ = new_Item;
+		return;
+	}
+	if (head_->first==0) { //when no space
+		Item* new_Item = new Item(); //create a new list
+		head_->prev = new_Item;
+		new_Item->next = head_;
+		head_ = new_Item;
+		new_Item->last = ARRSIZE;
+		new_Item->first = ARRSIZE-1;
+		new_Item->val[new_Item->first] = val;
+	}
+	else {
+		head_->val[head_->first-1] = val;
+		head_->first -= 1;
+	}
+	return;
+}
+
+void ULListStr::pop_back()
+{
+	//Removes a value from the back of the list
+	if (size_ == 0) {} // when empty list
+	else {
+		size_ -= 1;
+		tail_->last -= 1;
+		if (tail_->first == tail_->last) {//when end up empty
+			if (head_ == tail_) {//all list empty
+				delete tail_;
+				head_ = NULL; //update public head_, tail_
+				tail_ = NULL;
+			}
+			else if (head_ != tail_) {//only tail empty
+				Item* temp = tail_; //store empty tail_
+				tail_ = tail_->prev;
+				tail_->next = NULL;
+				delete temp;
+			}
+		}
+	}
+	return;
+}
+
+void ULListStr::pop_front()
+{
+	//Removes a value from the front of the list
+	if (size_ == 0) {} // when empty list
+	else {
+		size_ -= 1;
+		head_->first += 1;
+		if (tail_->first == tail_->last) {//when end up empty
+			if (head_ == tail_) {//all list empty
+				delete head_;
+				head_ = NULL; //update public head_, tail_
+				tail_ = NULL;
+			}
+			else if (head_ != tail_) {//only head empty
+				Item* temp = head_; //store empty head_
+				head_ = head_->next;
+				head_->prev = NULL;
+				delete temp;
+			}
+		}
+	}
+	return;
+}
+
+std::string const & ULListStr::back() const
+{
+	//Returns a const reference to the back element
+	int loc_back = tail_->last - 1;
+	return tail_->val[loc_back];
+}
+
+std::string const & ULListStr::front() const
+{
+	//Returns a const reference to the front element
+	int loc_front = head_->first;
+	return head_->val[loc_front];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+	//Returns a pointer to the item at index, loc,
+	//if loc is valid and NULL otherwise
+	if (loc >= size_) {//loc invalid
+		return NULL;
+	}
+	else if (loc < 0) {
+		return NULL;
+	}
+	else {//loc valid
+		Item* temp = head_;
+		unsigned int size_list = temp->last - temp->first;
+		while (loc+1 > size_list) {
+			loc -= size_list;
+			temp = temp->next;
+			size_list = temp->last - temp->first;
+		}
+		return temp->first+loc+temp->val;
+	}
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -2,10 +2,192 @@
 
 #include <iostream>
 using namespace std;
-
+#include "ulliststr.h"
 
 int main(int argc, char* argv[])
 {
+  cout << "getValAtLoc is tested within the public functions" << endl;
+	cout << "back and front's tests are involved in push and pop tests" << endl << endl;
+	//push_back
+	cout << "Test1: push_back, push to empty list" << endl;
+	ULListStr test1;
+	cout << "push 0 to the back" << endl;
+	test1.push_back("0");
+	cout << "	Test on back and front:" << endl;
+	cout << "		Expected back: 0 \n		Output back: " << test1.back() << endl;
+	cout << "		Expected front: 0 \n		Output front: " << test1.front() << endl;
+	cout << "push 1 to the back" << endl;
+	test1.push_back("1");
+	cout << "Expected: 0 1" << endl;
+	cout << "Output: " << test1.get(0) << " " << test1.get(1) << endl << endl;
+	//push_back
+	cout << "Test2: push_back, push to full list" << endl;
+	ULListStr test2;
+	cout << "Create a full list containing 0 to 9: ";
+	for (int i=0; i<10; i++) {
+		test2.push_back(to_string(i));
+	}
+	for (int i=0; i<10; i++) {
+		cout << test2.get(i) << " ";
+	}
+	cout << endl;
+	cout << "Total size is " << test2.size() << endl;
+	cout << "push 10 to the back, create a new list" << endl;
+	test2.push_back("10");
+	cout << "Expected: 0 1 2 3 4 5 6 7 8 9 10" << endl;
+	cout << "Output: ";
+	for (int i=0; i<11; i++) {
+		cout << test2.get(i) << " ";
+	}
+	cout << endl;
+	cout << "The new total size is " << test2.size() << endl << endl;
+	//push_front
+	cout << "Test3: push_front, push to empty list" << endl;
+	ULListStr test3;
+	cout << "push 0 to the front" << endl;
+	test3.push_front("0");
+	cout << "push 1 to the front" << endl;
+	test3.push_front("1");
+	cout << "Expected: 1 0" << endl;
+	cout << "Output: " << test3.get(0) << " " << test3.get(1) << endl << endl;
+	//push_front
+		cout << "Test4: push_front, push to full list" << endl;
+	ULListStr test4;
+	cout << "Create a full list containing 0 to 9: ";
+	for (int i=0; i<10; i++) {
+		test4.push_back(to_string(i));
+	}
+	for (int i=0; i<10; i++) {
+		cout << test4.get(i) << " ";
+	}
+	cout << endl;
+	cout << "Total size is " << test4.size() << endl;
+	cout << "push 10 to the front, create a new list" << endl;
+	test4.push_front("10");
+	cout << "Expected: 10 0 1 2 3 4 5 6 7 8 9" << endl;
+	cout << "Output: ";
+	for (int i=0; i<11; i++) {
+		cout << test4.get(i) << " ";
+	}
+	cout << endl;
+	cout << "The new total size is " << test4.size() << endl;
+	cout << "	Test on back and front:" << endl;
+	cout << "		Expected back: 9 \n		Output back: " << test4.back() << endl;
+	cout << "		Expected front: 10 \n		Output front: " << test4.front() << endl << endl;
+	//pop_back
+	cout << "Test5: pop_back, normal case" << endl;
+	cout << "Create a list containing 0 to 3" << endl;
+	ULListStr test5;
+	for (int i=0; i<4; i++) {
+		test5.push_back(to_string(i));
+	}
+	cout << "Current list: ";
+	for (int i=0; i<4; i++) {
+		cout << test5.get(i) << " ";
+	}
+	cout << endl;
+	cout << "pop on the back for once" << endl;
+	test5.pop_back();
+	cout << "Expected list: 0 1 2" << endl << "Output: ";
+	for (int i=0; i<3; i++) {
+		cout << test5.get(i) << " ";
+	}
+	cout << endl << "Total size: " << test5.size() << endl;
+	cout << "	Test on back and front:" << endl;
+	cout << "		Expected back: 2 \n		Output back: " << test5.back() << endl;
+	cout << "		Expected front: 0 \n		Output front: " << test5.front() << endl << endl;
+	//pop_back
+	cout << "Test6: pop_back, pop on an empty list" << endl;
+	ULListStr test6;
+	cout << "Create an empty list" << endl;
+	cout << "Current size: " << test6.size() << endl;
+	cout << "pop on the back for once" << endl;
+	test6.pop_back();
+	cout << "Expected size: 0" << endl;
+	cout << "Output size: " << test6.size() << endl << endl;
+	//pop_back
+	cout << "Test7: pop_back, pop on a non-empty list but end up empty" << endl;
+	ULListStr test7;
+	cout << "Create a list containing 0 and 1" << endl;
+	test7.push_back("0");
+	test7.push_back("1");
+	cout << "Current list: " << test7.get(0) << " " << test7.get(1) << endl;
+	cout << "pop on the back for once" << endl;
+	test7.pop_back();
+	cout << "Current list size: " << test7.size() << endl;
+		cout << "Current list: " << test7.get(0) << endl;
+	cout << "pop on the back for once" << endl;
+	test7.pop_back();
+	cout << "Expected list size: 0 (empty)" << endl;
+	cout << "Output size: " << test7.size() << endl << endl;
+	//pop_front
+	cout << "Test8: pop_front, normal case" << endl;
+	cout << "Create a list containing 0 to 3" << endl;
+	ULListStr test8;
+	for (int i=0; i<4; i++) {
+		test8.push_back(to_string(i));
+	}
+	cout << "Current list: ";
+	for (int i=0; i<4; i++) {
+		cout << test8.get(i) << " ";
+	}
+	cout << endl;
+	cout << "pop on the front for once" << endl;
+	test8.pop_front();
+	cout << "Expected list: 1 2 3" << endl << "Output: ";
+	for (int i=0; i<3; i++) {
+		cout << test8.get(i) << " ";
+	}
+	cout << endl << "Total size: " << test8.size() << endl << endl;
+	//pop_front
+	cout << "Test9: pop_front, pop on an empty list" << endl;
+	ULListStr test9;
+	cout << "Create an empty list" << endl;
+	cout << "Current size: " << test9.size() << endl;
+	cout << "pop on the back for once" << endl;
+	test9.pop_back();
+	cout << "Expected size: 0" << endl;
+	cout << "Output size: " << test9.size() << endl << endl;
+	//pop_front
+	cout << "Test10: pop_front, pop on a non-empty list but end up empty" << endl;
+	ULListStr test10;
+	cout << "Create a list containing 0 and 1" << endl;
+	test10.push_back("0");
+	test10.push_back("1");
+	cout << "Current list: " << test10.get(0) << " " << test10.get(1) << endl;
+	cout << "pop on the front for once" << endl;
+	test10.pop_front();
+	cout << "Current list size: " << test10.size() << endl;
+	cout << "Current list: " << test10.get(0) << endl;
+	cout << "pop on the front for once" << endl;
+	test10.pop_front();
+	cout << "Expected list size: 0 (empty)" << endl;
+	cout << "Output size: " << test10.size() << endl << endl;
+	//all together
+	cout << "Test0: push_back, push_front, pop_back, pop_front" << endl;
+	ULListStr test0;
+	cout << "Create an empty list" << endl;
+	cout << "push_back Autumn" << endl;
+	test0.push_back("Autumn");
+	cout << "push_front Mid" << endl;
+	test0.push_front("Mid");
+	cout << "push_front Happy" << endl;
+	test0.push_front("Happy");
+	cout << "push_back Festival" << endl;
+	test0.push_back("Festival");
+	cout << "Output: ";
+	for (int i=0; i<4; i++) {
+		cout << test0.get(i) << " ";
+	}
+	cout << endl << "pop_front" << endl;
+	test0.pop_front();
+	cout << "pop_back" << endl;
+	test0.pop_back();
+	cout << "Output: ";
+	for (int i=0; i<2; i++) {
+		cout << test0.get(i) << " ";
+	}
+	cout << endl;
 
   return 0;
 }
